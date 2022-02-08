7e23202219d55fe7f2c4c2295f720b946abf5458
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-02-08 11:41:59 +0330

    register User,Profile,OtpRequest model to the django admin

--- backend/getotp/admin.py
+++ backend/getotp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import OtpRequest,User,Profile
 # Register your models here.
+
+admin.site.register(OtpRequest)
+admin.site.register(User)
+admin.site.register(Profile)
