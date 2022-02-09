b6f9bdcc46621f2d7d458ceb4de3aba19ded3e3c
Matin Dezhbani <dezhbanimatin@gmail.com>
2022-02-09 16:10:50 +0330

    add state and setState & pass to sign up - verify component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Verify from './components/Verify';
+import './App.css';
+
 
 //Components
 import Signup from './components/Signup';
 
 function App() {
+
+  const [state, setState] = useState({});
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Navigate to="/sign-up"/>} />
-        <Route path="/sign-up" element={<Signup/>} />
+        <Route path="/sign-up" element={<Signup state={state} setState={setState} />} />
+        <Route path="/sign-up/verify" element={<Verify state={state}/>}/>
       </Routes>
     </div>
   );
