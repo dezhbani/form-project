6d689fd8dc2c12377b778a5e49930dcc76371eb4
Matin Dezhbani <dezhbanimatin@gmail.com>
2022-02-10 20:57:45 +0330

    change component

--- frontend/src/components/Verify.js
+++ frontend/src/components/Verify.js
@@ -1,42 +1,69 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import axios from 'axios';
+import { validate } from './validate';
+import { ToastContainer } from 'react-toastify';
+import { notify } from './toast';
+
+//style
+import 'react-toastify/dist/ReactToastify.css';
+import style from './Verify.module.css';
 
 const Verify = ({state}) => {
 
-    const [data, setData] = useState({
-        code1:"", 
-        code2:"", 
-        code3:"", 
-        code4:""
-        });
+    const [data, setData] = useState({code1: ""});
+    const [erorrs, setErorrs] = useState({});
+    const [touched, setTouched] = useState({});
+
+    console.log(Object.keys(erorrs).length);
+
+    useEffect(() =>{
+        setErorrs(validate(data, "verify"));
+        console.log(erorrs)
+    }, [data, touched]);
+
+    const focus = () =>{
+        setTouched({ code: true });
+    }
+
 
     const change = event =>{
-        setData({...data, [event.target.name]: event.target.value});
-        console.log({...data, [event.target.name]: event.target.value})
+        setData({[event.target.name]: event.target.value});
     }
-    
-    
+
     const send = async () =>{
-        const code = data.code1 + data.code2 + data.code3 + data.code4
-        // console.log(state.length)
-        console.log(typeof state.id);
-        console.log(typeof state.phone);
-        if(code.length === 4){
+        if(!Object.keys(erorrs).length){
             const res = await axios.post("http://127.0.0.1:8000/otp/verify/",{
                 request_id: state.id,
                 phone: state.phone,
-                password: code
+                password: data.code1
             });
-            console.log(res);
+            console.log(res.data.new_user);
+            if(res.data.new_user){
+                notify("success", "ثبت نامت تکمیل شد")
+            }else{ 
+                console.log(res);
+                notify("success", "خوش آمدی");
         }
+        }else{
+            setTouched({code: true});
+            notify("error", "لطفا فرم رو کامل کن");
+        }
+
     }
+
     return (
-        <div>
-            <input maxLength="1" onChange={change} name='code1' value={data.code1} type="text" />
-            <input maxLength="1" onChange={change} name='code2' value={data.code2} type="text" />
-            <input maxLength="1" onChange={change} name='code3' value={data.code3} type="text" />
-            <input maxLength="1" onChange={change} name='code4' value={data.code4} type="text" />
-            <button type="submit" onClick={send}>send</button>
+        <div className={style.maincontainer}>
+            <div className={style.container}>
+                <div className={style.form}>
+                    <label className={style.label}>کد تایید:</label>
+                    <input className={(touched.code && erorrs.code)? style.code : style.uncompleted} maxLength="4" onChange={change}  onFocus={focus} name='code1' value={data.code1} type="text" />
+                    {erorrs.code && touched.code && <span>{erorrs.code}</span>}
+                    <div className={style.button}>
+                        <button type="submit" onClick={send}>send</button>
+                    </div>
+                </div>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
