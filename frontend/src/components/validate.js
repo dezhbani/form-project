91bb52337f97c45fda4d264b7097e09770a79cc9
Matin Dezhbani <dezhbanimatin@gmail.com>
2022-02-10 10:09:33 +0330

    change validate.js

--- frontend/src/components/validate.js
+++ frontend/src/components/validate.js
@@ -1,20 +1,22 @@
-export const validat = (data) =>{
+export const validate = (data, type) =>{
 
     const erorrs = {};
 
-    if(data.phone){
-        erorrs.phone = "شماره تلفن نمیتونه خالی باشه";
-    }else if(/\D/.test(data.phone)){
-        erorrs.phone = "شماره تلفن باید فقط اعداد باشه";
-    }else{
-        delete erorrs.phone
-    };
+        if(!data.phone){
+            erorrs.phone = "شماره تلفن نمیتونه خالی باشه";
+        }else if(/\D/.test(data.phone)){
+            erorrs.phone = "شماره تلفن باید فقط اعداد باشه";
+        }else{
+            delete erorrs.phone
+        };
 
-    if(data.code){
-        erorrs.code = "کد تایید نمیتونه خالی باشه";
-    }else if(/\D/.test(data.code)){
-        erorrs.code = "کد تایید باید فقط اعداد باشه";
-    };
+    if(type === "verify"){
+        if(!data.code){
+            erorrs.code = "کد تایید نمیتونه خالی باشه";
+        }else if(/\D/.test(data.code)){
+            erorrs.code = "کد تایید باید فقط اعداد باشه";
+        };
+    }
 
     return erorrs;
 }
