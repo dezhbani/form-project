c444e499828e21c5d5349f79a00e65d9e9ac3431
Matin Dezhbani <dezhbanimatin@gmail.com>
2022-02-10 13:49:44 +0330

    add toastify & change style

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { validate } from './validate';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import { notify } from './toast';
 
 //styles
+import 'react-toastify/dist/ReactToastify.css';
 import style from'./Signup.module.css';
 
 const Signup = ({state, setState}) => {
@@ -15,15 +18,15 @@ const Signup = ({state, setState}) => {
 
     console.log(Object.keys(erorrs).length);
 
-    // useEffect(() =>{
-    //     setErorrs(validate(data, "signup"));
-    //     console.log(erorrs)
-    // }, [data, touched]);
+    useEffect(() =>{
+        setErorrs(validate(data, "signup"));
+        console.log(erorrs)
+    }, [data, touched]);
 
     const focus = event =>{
         setTouched({ ...touched, [event.target.name]: true });
-        setErorrs(validate(data));
-        console.log(erorrs);
+        // setErorrs(validate(data));
+        // console.log(erorrs);
     }
 
     const change = event =>{
@@ -31,7 +34,7 @@ const Signup = ({state, setState}) => {
     }
 
     const clicked = async () =>{
-        if(Object.keys(erorrs).length === 2){
+        if(!Object.keys(erorrs).length){
             const res = await axios.post("http://127.0.0.1:8000/otp/request/",{
                 phone: data.phone
             });
@@ -40,12 +43,17 @@ const Signup = ({state, setState}) => {
             console.log(state);
     
             if(res.status === 200){
-                navigate('/sign-up/verify');
+                notify("success", "کد تایید ارسال شد")
+                const navigat = () =>{
+                    navigate('/sign-up/verify')
+                }
+                setTimeout(navigat, 5500);
             };
-            
-        }else{ setTouched({
-            phone: true
-        })}
+
+        }else{ 
+            setTouched({phone: true});
+            notify("error", "لطفا فرم را کامل کنید");
+    };
 
     }
 
@@ -58,11 +66,14 @@ const Signup = ({state, setState}) => {
                 <div className={style.formfield}>
                     <label className={style.label}>شماره تلفن:</label>
                     <input className={(touched.phone && erorrs.phone)? style.uncompleted : style.phone} type="tel" value={data.phone} onFocus={focus} onChange={change} name='phone' placeholder='شماره تلفن' />
+                    {erorrs.phone && touched.phone && <span>{erorrs.phone}</span>}
+
                     <div className={style.buttons}>
                         <button onClick={clicked} type="submit">تایید</button>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
